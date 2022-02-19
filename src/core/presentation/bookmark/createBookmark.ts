7df30a6661a2984545fc89adb4ce5313e13bee41
dojunho <djh20@kumoh.ac.kr>
2022-02-19 19:24:26 +0900

    add list all api

--- src/core/presentation/bookmark/createBookmark.ts
+++ src/core/presentation/bookmark/createBookmark.ts
@@ -1,7 +1,11 @@
 import {Bookmark} from "src/core/domain/bookmark/bookmark";
-import {createBookmarkUsecase} from "src/core/application/bookmark/createBookmark";
+import {createBookmarkUsecase, listAllBookmarksUsecase} from "src/core/application/bookmark/bookmarkUsecase";
 
 
-export async function createBookmark(parentID: string | undefined, title: string, url: string | undefined): Promise<Bookmark | Error>{
-    return createBookmarkUsecase(parentID, title, url)
+export async function createBookmark(parentID: string | undefined, title: string, url: string | undefined): Promise<Bookmark | Error> {
+    return await createBookmarkUsecase(parentID, title, url)
+}
+
+export async function listAllBookmarks(): Promise<Bookmark[] | Error>{
+    return await listAllBookmarksUsecase()
 }
