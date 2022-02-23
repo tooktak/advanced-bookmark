a8f0ce61d72e053e1e3205d741b36e910ed4ed8a
hwanfront <kim015jh@gmail.com>
2022-02-24 00:40:21 +0900

    add bookmark list test component with storage

--- src/background.ts
+++ src/background.ts
@@ -3,5 +3,9 @@ import {Bookmark} from "src/core/domain/bookmark/bookmark";
 
 (async () => {
     const bookmark: Bookmark[] | Error = await listAllBookmarks()
-    console.log(bookmark)
+    console.log(bookmark);
+    // 북마크 정보 chrome storage 에 저장
+    chrome.storage.local.set({bookmark}, function() {
+        console.log('Value is set to ' + bookmark);
+    });
 })();
---
+++ src/client/component/BookmarkListItem.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Bookmark } from "src/core/domain/bookmark/bookmark";
+
+interface BookmarkListItemProps {
+  bookmark: Bookmark
+}
+
+const BookmarkListItem: React.FC<BookmarkListItemProps> = ({ bookmark }) => {
+  useEffect(() => {
+    console.log(bookmark)
+  }, [bookmark])
+
+  return(
+    <li>
+      <span>{bookmark.parentId}</span>
+      <span>{bookmark.id} / </span>
+      <span>{bookmark.title} / </span>
+      <p>{bookmark.url}</p>
+    </li>
+  );
+}
+
+export default BookmarkListItem;
