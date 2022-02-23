a99f7821c9072a7e8b648972abe4629996db34cd
dldhk97 <dldhk97@gmail.com>
2022-02-23 12:41:03 +0900

    Add BookmarkReadDto in presentation layer

---
+++ src/core/presentation/dto/bookmarkReadDto.ts
@@ -0,0 +1,38 @@
+import {Bookmark} from "src/core/domain/bookmark/bookmark";
+
+export class BookmarkReadDto {
+  private _parentId?: string;
+  private _id: string;
+  private _title: string;
+  private _url?: string;
+
+  constructor(parentID: string | undefined, id: string, title: string, url: string | undefined) {
+    this._parentId = parentID
+    this._id = id;
+    this._title = title;
+    this._url = url;
+  }
+
+  get parentId(): string | undefined {
+    return this._parentId;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  get url(): string | undefined{
+    return this._url;
+  }
+
+  static fromEntity(bookmark : Bookmark){
+    if (bookmark.id === undefined)
+      throw new Error('Bookmark id is undefined')
+    return new BookmarkReadDto(bookmark.parentId, bookmark.id, bookmark.title, bookmark.url);
+  }
+
+}
