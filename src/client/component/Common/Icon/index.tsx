3cb628d61ac3af1acb121c3a56ece128aed94147
hwanfront <kim015jh@gmail.com>
2022-03-11 02:17:21 +0900

    add export icon components and icon's props

---
+++ src/client/component/Common/Icon/index.tsx
@@ -0,0 +1,41 @@
+import AddIcon from "./AddIcon";
+import ArrowIcon from "./ArrowIcon";
+import BackupIcon from "./BackupIcon";
+import BookmarkIcon from "./BookmarkIcon";
+import CheckboxEmptyIcon from "./CheckboxEmptyIcon";
+import CheckboxFilledIcon from "./CheckboxFilledIcon";
+import CloseIcon from "./CloseIcon";
+import DragIcon from "./DragIcon";
+import FolderIcon from "./FolderIcon";
+import MenuIcon from "./MenuIcon";
+import OptionIcon from "./OptionIcon";
+import RefreshIcon from "./RefreshIcon";
+import RestoreBackupIcon from "./RestoreBackupIcon";
+import RestoreBookmarkIcon from "./RestoreBookmarkIcon";
+import SearchIcon from "./SearchIcon";
+import SeeMoreIcon from "./SeeMoreIcon";
+import SelectCheckboxIcon from "./SelectCheckboxIcon";
+import SortIcon from "./SortIcon";
+import StarIcon from "./StarIcon";
+
+export { 
+  AddIcon, 
+  ArrowIcon, 
+  BackupIcon, 
+  BookmarkIcon, 
+  CheckboxEmptyIcon, 
+  CheckboxFilledIcon, 
+  CloseIcon, 
+  DragIcon, 
+  FolderIcon, 
+  MenuIcon, 
+  OptionIcon, 
+  RefreshIcon, 
+  RestoreBackupIcon, 
+  RestoreBookmarkIcon, 
+  SearchIcon, 
+  SeeMoreIcon, 
+  SelectCheckboxIcon, 
+  SortIcon, 
+  StarIcon 
+};
