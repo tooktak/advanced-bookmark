98cd05c2ba55445be12af3e0fc000e16963c1fbd
hwanfront <kim015jh@gmail.com>
2022-03-13 00:26:43 +0900

    fix react attribute name error

--- src/client/component/Common/Icon/CheckboxFilledIcon/index.tsx
+++ src/client/component/Common/Icon/CheckboxFilledIcon/index.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const CheckboxFilledIcon: React.FC<IconProps> = ({ size="24", color = 'black' }) => {
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="5" y="5" width="14" height="14" rx="2" stroke={color} stroke-width="2"/>
-      <path d="M7.5 12.2353L10.7727 16L13.6364 12L16.5 8" stroke={color} stroke-width="2" stroke-linecap="round"/>
+      <rect x="5" y="5" width="14" height="14" rx="2" stroke={color} strokeWidth="2"/>
+      <path d="M7.5 12.2353L10.7727 16L13.6364 12L16.5 8" stroke={color} strokeWidth="2" strokeLinecap="round"/>
     </svg>
   );
 };
