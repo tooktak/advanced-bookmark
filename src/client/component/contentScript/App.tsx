4671c7594fbf1f7f42b4c5c80748b187c58fd93d
hwanfront <kim015jh@gmail.com>
2022-03-19 23:52:17 +0900

    add  bookmarkList state

--- src/client/component/contentScript/App.tsx
+++ src/client/component/contentScript/App.tsx
@@ -1,6 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import BookmarkSidebar from "./BookmarkSidebar/BookmarkSidebar";
 import BookmarkIndicator from "./BookmarkIndicator/BookmarkIndicator";
+
+type Bookmark = {
+  _parentId: string | undefined;
+  _id: string | undefined;
+  _title: string;
+  _url: string | undefined;
+}
+
+type BookmarkListType = {
+  bookmarkList: Bookmark[];
+  getBookmarkList(): void;
+}
 
 type BookmarkSidebarOpen = {
   open: boolean;
@@ -9,9 +21,20 @@ type BookmarkSidebarOpen = {
 }
 
 export const BookmarkOpen = createContext<BookmarkSidebarOpen>({} as BookmarkSidebarOpen)
+export const BookmarkList = createContext<BookmarkListType>({} as BookmarkListType)
 
 const App: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const [bookmarkList, setBookmarkList] = useState<Bookmark[]>([]);
+
+  const getBookmarkList = () => {
+    // 북마크에 저장된 데이터 chrome storage 에서 불러오기
+    chrome.storage.local.get(['bookmark'], (result) => {
+      // storage 에서 불러온 데이터는 Object 형`태 => Bookmark 로 변환
+      const res = result.bookmark.filter((e: Bookmark) => e._url);
+      setBookmarkList(res);
+    });
+  }
 
   const handleOpen = () => {
     setOpen(true);
@@ -21,10 +44,16 @@ const App: React.FC = () => {
     setOpen(false);
   }
 
+  useEffect(() => {
+    getBookmarkList();
+  }, [])
+
   return (
     <BookmarkOpen.Provider value={{open, handleOpen, handleClose}}>
-      <BookmarkSidebar />
-      <BookmarkIndicator />
+      <BookmarkList.Provider value={{bookmarkList, getBookmarkList}}>
+        <BookmarkSidebar />
+        <BookmarkIndicator />
+      </BookmarkList.Provider>
     </BookmarkOpen.Provider>
   );
 };
