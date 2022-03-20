d366547fd5cc61ba7951c64907a2331ebb508d81
hwanfront <kim015jh@gmail.com>
2022-03-20 23:55:29 +0900

    add bookmark option

--- src/client/component/contentScript/BookmarkSidebar/BookmarkOption/BookmarkOption.tsx
+++ src/client/component/contentScript/BookmarkSidebar/BookmarkOption/BookmarkOption.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from './BookmarkOption.module.css';
+import Checkbox from "src/client/component/Common/Checkbox/Checkbox";
+import { ArrowIcon } from "src/client/component/Common/Icon";
+import { Store } from "src/client/component/contentScript/App";
 
 const BookmarkOption: React.FC = () => {
+  const { open, showFolder, orderBy, handleShowFolder, handleOrderBy } = useContext(Store.BookmarkOption);
+
+
+
   return (
-    <div className={styles.bookmarkOption}>
-      option
+    <div className={`${styles.bookmarkOption} ${open ? styles.open : ''}`}>
+      <a className={orderBy === "ASC" ? styles.asc : styles.desc} onClick={handleOrderBy}>
+        <ArrowIcon size="18" />
+        <span>정렬</span>
+      </a>
+      <Checkbox
+        checked={showFolder}
+        onClick={handleShowFolder}
+        id="showFolder"
+        text="폴더 보기"
+      />
     </div>
   );
 };
