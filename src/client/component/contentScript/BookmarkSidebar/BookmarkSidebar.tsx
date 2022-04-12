06c72de4fb619392f52a5d5a64139a9200029d89
hwanfront <kim015jh@gmail.com>
2022-04-12 23:32:35 +0900

    add option scroll event, 옵션 스크롤 내리면 close 올리면 open

--- src/client/component/contentScript/BookmarkSidebar/BookmarkSidebar.tsx
+++ src/client/component/contentScript/BookmarkSidebar/BookmarkSidebar.tsx
@@ -1,40 +1,24 @@
-import React, { UIEvent, useState } from "react";
+import React, { useRef } from "react";
 import styles from "./BookmarkSidebar.module.css";
 import BookmarkHeader from "./BookmarkHeader/BookmarkHeader";
 import BookmarkList from "./BookmarkList/BookmarkList";
 import BookmarkOption from "./BookmarkOption/BookmarkOption";
 import Background from "src/client/component/Common/Background/Background"
 import { useBookmarkSidebar } from "src/client/context/UiContext";
-
-type Scroll = {
-  prev: number;
-  option: boolean;
-}
+import useOptionOpenWithScroll from "src/client/component/contentScript/BookmarkSidebar/useOptionOpenWithScroll";
 
 const BookmarkSidebar: React.FC = () => {
-  const [scroll, setScroll] = useState<Scroll>({
-    prev: 0,
-    option: true
-  });
   const { open, handleSidebarClose } = useBookmarkSidebar();
-
-  const handleScroll = (e: UIEvent<HTMLDivElement>) => {
-    const { scrollTop } = e.currentTarget;
-    const { prev } = scroll;
-    scrollTop > prev ? (
-      setScroll({ prev: scrollTop, option: false })
-    ) : (
-      setScroll({ prev: scrollTop, option: true })
-    )
-  }
+  const bookmarkListRef = useRef<HTMLDivElement>(null);
+  const optionOpen = useOptionOpenWithScroll(bookmarkListRef, 200);
 
   return (
     <>
       <Background open={open} handleClose={handleSidebarClose} />
       <aside className={styles.bookmarkSidebar}>
         <BookmarkHeader />
-        <BookmarkOption option={scroll.option} />
-        <div onScroll={handleScroll} className={styles.listScroll}>
+        <BookmarkOption option={optionOpen} />
+        <div ref={bookmarkListRef} className={styles.listScroll}>
           <BookmarkList />
         </div>
       </aside>
